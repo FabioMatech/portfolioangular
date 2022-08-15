64714771e242e8858391a03572df880736d67963
Fabio <clubeteczagem@gmail.com>
2022-08-15 11:54:49 -0300

    criação do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
