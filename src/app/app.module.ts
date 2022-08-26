4b1701242d8b9ec9b2bcb50d11f97f8787d36d26
Fabio <clubeteczagem@gmail.com>
2022-08-26 09:22:09 -0300

    inserindo locale e currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,8 +7,10 @@ import { HeaderComponent } from './header/header.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FooterComponent } from './footer/footer.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localPt from  '@angular/common/locales/pt'
 
-
+registerLocaleData(localPt, 'pt')
 
 @NgModule({
   declarations: [
@@ -23,7 +25,17 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function localPT(localPT: any, arg1: string) {
+  throw new Error('Function not implemented.');
+}
+
