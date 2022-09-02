5e2661d94e4f369c635114997289a3699032c339
Fabio <clubeteczagem@gmail.com>
2022-09-02 06:20:15 -0300

    Aula 6: Nova-transferencia.ts

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,36 +1,26 @@
-import { TransferenciaService } from './../transferencia.service';
 import { Component, Output, EventEmitter } from "@angular/core";
-import { Transferencia } from '../models/transferencia.models';
-
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-
-
-export class NovaTransferenciaComponent{
-
+export class NovaTransferenciaComponent {
   @Output() aoTransferir = new EventEmitter<any>();
 
-  valor!: number;
-  destino!: number;
+  valor: number;
+  destino: number;
 
-  constructor(private service: TransferenciaService){}
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
 
-  transferir(){
-    console.log("Solicitada nova transferência");
-    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
-    this.service.adicionar(valorEmitir).subscribe(resultado =>  {
-      console.log(resultado);
-      //this.limparCampos();
-    },
-    error =>  console.error(error));
+    this.limparCampos();
   }
 
- // limparCampos(){
- //   this.valor = 0;
- //   this.destino = 0;
- // }
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
